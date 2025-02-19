607063d1b89deb7405b27fde334dc49f4840476e
Novakboh-step <novak_dv82@student.itstep.org>
2025-02-19 09:31:28 +0200

    Added class Numbers
    This class contains two numbers

---
+++ Numbers.py
@@ -0,0 +1,10 @@
+class Numbers:
+    def __init__(self, a, b):
+        self.__a = a
+        self.__b = b
+
+    def getA(self):
+        return self.__a
+
+    def getB(self):
+        return self.__b
